58accf1c0a03f1d5ecb26aebe79db0b82f8f779d
jw <josephwhyman@gmail.com>
2023-06-19 21:02:12 +0100

    Update Configuration_adv.h

--- Motherboard Firmware/Marlin/Configuration_adv.h
+++ Motherboard Firmware/Marlin/Configuration_adv.h
@@ -279,7 +279,7 @@
 #if TEMP_SENSOR_BOARD
   #define THERMAL_PROTECTION_BOARD   // Halt the printer if the board sensor leaves the temp range below.
   #define BOARD_MINTEMP           8  // (°C)
-  #define BOARD_MAXTEMP          70  // (°C)
+  #define BOARD_MAXTEMP          80  // (°C)
   #ifndef TEMP_BOARD_PIN
     //#define TEMP_BOARD_PIN -1      // Board temp sensor pin, if not set in pins file.
   #endif
